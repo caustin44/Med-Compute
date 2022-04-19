b1ee4f080dc41d4abbc431d47f3b29035a36abf7
Christopher Austin <chris.austin@medica3d.com.au>
2022-04-19 12:12:42 +1000

    Modified grasshopper endpoint to return correct data - and encode resulting mesh as 3dm file before transferring to client

--- src/compute.geometry/FixedEndpoints.cs
+++ src/compute.geometry/FixedEndpoints.cs
@@ -9,6 +9,7 @@ namespace compute.geometry
     public class ConvertModel
     {
         public string b64str { get; set; }
+        public string extension { get; set; }
     }
 
     public class FixedEndPointsModule : NancyModule
@@ -18,28 +19,27 @@ namespace compute.geometry
             Get[""] = _ => HomePage(Context);
             Get["/healthcheck"] = _ => "healthy";
             Get["version"] = _ => GetVersion(Context);
-            Post["convert"] = _ => ConvertObj(Context);
+            Post["convert"] = _ => ConvertTo3DM(Context);
             Post["encode"] = _ => EncodeFile(Context);
             Get["servertime"] = _ => ServerTime(Context);
             Get["sdk/csharp"] = _ => CSharpSdk(Context);
         }
-
         static Response HomePage(NancyContext ctx)
         {
             return new Nancy.Responses.RedirectResponse("https://www.rhino3d.com/compute");
         }
 
-        static Response ConvertObj(NancyContext ctx)
+        static Response ConvertTo3DM(NancyContext ctx)
         {
             using (var doc = Rhino.RhinoDoc.CreateHeadless(null))
             {
                 // Decode the obj file and create an instance of it on the serverside
                 var requestBody = ctx.Request.Body.AsString();
                 ConvertModel jsonObject = Newtonsoft.Json.JsonConvert.DeserializeObject<ConvertModel>(requestBody);
-                System.IO.File.WriteAllBytes("temp.obj", Convert.FromBase64String(jsonObject.b64str));
+                System.IO.File.WriteAllBytes("temp." + jsonObject.extension, Convert.FromBase64String(jsonObject.b64str));
 
                 // Create a rhino document based on the object file, and export it as a .3dm
-                doc.Import("temp.obj");
+                doc.Import("temp." + jsonObject.extension);
                 doc.Write3dmFile("temp.3dm", new Rhino.FileIO.FileWriteOptions());
 
                 // Write the document to an instance of Rhino's File3DM so that it can be returned as a byte array
@@ -67,7 +67,7 @@ namespace compute.geometry
             var values = new Dictionary<string, string>();
             values.Add("rhino", Rhino.RhinoApp.Version.ToString());
             values.Add("compute", Assembly.GetExecutingAssembly().GetName().Version.ToString());
-            string git_sha = null; // appveyor will replace this
+            string git_sha = null;
             values.Add("git_sha", git_sha);
             var response = (Nancy.Response)Newtonsoft.Json.JsonConvert.SerializeObject(values);
             response.ContentType = "application/json";
